ed16921d4d800d39dcc517b69ba2a9b67123a3ff
Benjamin Coudrin <benjamin.coudrin@gmail.com>
2022-07-16 11:11:59 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Canvas from './components/Canvas/Canvas';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Canvas/Canvas.js
@@ -0,0 +1,18 @@
+import React, { useRef, useEffect } from 'react';
+import "./Canvas.css";
+
+const Canvas = props => {
+    
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        context.fillStyle = "#0000FF";
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }, []);
+
+    return <canvas className="canvasArea" ref={canvasRef} {...props} />
+}
+
+export default Canvas;
