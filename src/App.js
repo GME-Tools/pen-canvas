17660c833a10cc6dfb607319c9d01280b7380399
Benjamin Coudrin <benjamin.coudrin@gmail.com>
2022-07-20 10:01:25 +0200

    Add authentication and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+import { useAuth } from "context/UserContext";
+
+import SignIn from "components/SignIn/SignIn";
+import CanvasList from "components/CanvasList/CanvasList";
 import Canvas from './components/Canvas/Canvas';
+
 import './App.css';
+
+
+function SignInRoute() {
+  const auth = useAuth();
+  if (auth.authUser) {
+    return <Navigate to="/list" replace />
+  }
+  return <SignIn />
+}
+
+function CanvasRoute() {
+  let auth = useAuth();
+  if (!auth.authUser) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <CanvasList />
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<SignInRoute />} />
+      <Route path="/list" element={<CanvasRoute />} />
+      <Route path="/:id" element={<Canvas />} />
+      <Route path="/:id/:token" element={<Canvas />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Canvas />
+      <Router>
+      <div className="App">
+        <CssBaseline />
+        <AppRoutes />
+      </div>
+    </Router>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/CanvasList/CanvasList.js
@@ -0,0 +1,108 @@
+import { useFirebase } from "context/FirebaseContext";
+import { useEffect, useState } from "react";
+import { Button, Card, Container, IconButton, Snackbar, Typography } from "@mui/material"
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { Dialog, DialogActions, DialogContent } from '@mui/material';
+import { DialogContentText, DialogTitle } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function CanvasList() {
+  const firebase = useFirebase();
+  const [canvas, setCanvas] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [openSnack, setOpenSnack] = useState(false);
+  const [newUuid, setNewUuid] = useState("");
+
+  useEffect(() => {
+    firebase.getCurrentUser().then(doc => {
+      if (doc.data().hasOwnProperty('canvas')) {
+        setCanvas(doc.data().canvas);
+      }
+      else {
+        setCanvas([]);
+      }
+    })
+    .catch(err => {
+      firebase.setCurrentUserData({canvas: []})
+    });
+  }, [firebase]);
+
+  useEffect(() => {
+    if (newUuid !== "") {
+      setOpen(true);
+    }
+  },[newUuid])
+
+  const handleClose = () => setOpen(false);
+
+  const handleNew = () => {
+    const uid = uuidv4();
+    const data = [...canvas, uid]
+    setCanvas(data);
+    firebase.setCurrentUserData({canvas: data});
+    firebase.setDocument("canvas",uid,{});
+    setNewUuid(uid);
+  }
+
+  const handleClickList = elem => {
+    navigator.clipboard.writeText("https://pen-canvas.web.app/"+elem+'/'+firebase.authUser().uid)
+    setOpenSnack(true);
+  }
+
+  const handleDelete = elem => {
+    const data = canvas.filter(canvas => canvas !== elem);
+    setCanvas(data)
+    firebase.setCurrentUserData({canvas: data});
+    firebase.deleteDocument("canvas",elem);
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h5">Pen Canvas</Typography>
+      <Button onClick={handleNew}>Nouveau</Button>
+      <Card>
+        <List>
+        {
+          canvas.map(elem => (
+            <ListItem key={elem}
+              secondaryAction={
+                <IconButton edge="end" onClick={() => handleDelete(elem)}>
+                  <DeleteIcon />
+                </IconButton>
+              }
+              disablePadding
+            >
+              <ListItemButton onClick={() => handleClickList(elem)} dense>
+                <ListItemText>{elem}</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))
+        }
+        </List>
+      </Card>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>{"Nouveau Canvas"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            <Typography variant="body1"><b>Edition :</b> https://pen-canvas.web.app/{newUuid}/{firebase.authUser().uid}</Typography>
+            <Typography variant="body1"><b>Lecture seule :</b> https://pen-canvas.web.app/{newUuid}</Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>OK</Button>
+        </DialogActions>
+      </Dialog>
+      <Snackbar
+        open={openSnack}
+        autoHideDuration={5000}
+        onClose={()=>setOpenSnack(false)}
+        message="Lien copié"
+      />
+    </Container>
+  )
+}
