b8c5259c00ef050433ac80df738b129fe1353215
Benjamin Coudrin <benjamin.coudrin@gmail.com>
2022-07-20 13:22:17 +0200

    Init canvas

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -8,8 +8,8 @@ const Canvas = props => {
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        context.fillStyle = "#0000FF";
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        //context.fillStyle = "#0000FF";
+        //context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     }, []);
 
     return <canvas className="canvasArea" ref={canvasRef} {...props} />
